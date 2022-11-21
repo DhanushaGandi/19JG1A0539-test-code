e046306e3f85a6b5e6648122dc8edc66ebfcba0b
Gandi_Dhanusha <85733216+DhanushaGandi@users.noreply.github.com>
2022-11-21 16:58:53 +0530

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -2,7 +2,7 @@ validnumbers=['1','2','5','6','8','9','0']
 number=int(input())
 m=0
 kk=[]
-for i in range(100):
+for i in range(1000):
     for j in str(i):
         if j not in validnumbers:
             break
